d2ca2468d6472705587f23eca5aa705853cce317
pratik-sapkota007 <pratik.sapkota@shell.com>
2022-08-19 17:33:55 +0530

    Update gh action & tf file

--- README.md
+++ README.md
@@ -3,13 +3,16 @@ Nepal Cloud Meetup - August 2022
 
 ## Azure IaC with GitHub Actions and Terraform
 
-#GitHub Actions
+# GitHub Actions
 
  
 - automate build, test and deployment of code through the use of workflows
 - CI/CD tool
 - Code is stored in GitHub repositories
 - Marketplace of actions to use in your workflows
+
+
+
 
 
 
---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.8.0"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "pratik-demo-resource-group"
+    storage_account_name = "tfstate0029"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate2"
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+resource "azurerm_resource_group" "example" {
+
+  name = "ncp-devops-test"
+
+  location = "centralindia"
+}
