63e8604ad131bc0d90d59611a54f1ab1e58c9732
pratik-sapkota007 <pratik.sapkota@shell.com>
2022-08-20 17:00:01 +0530

    Add change to rg

--- main.tf
+++ main.tf
@@ -21,9 +21,18 @@ provider "azurerm" {
 
   }
 }
+# Create a resource group
 resource "azurerm_resource_group" "example" {
 
-  name = "ncp-devops-test1"
+  name = "ncp-devops-2022"
 
   location = "centralindia"
 }
+
+# # Create a virtual network within the resource group
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   address_space       = ["10.0.0.0/16"]
+# }
