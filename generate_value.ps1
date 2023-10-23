5408ad6e159206db611e405d9d12d60611e617af
Pratik Sapkota <sapkotapratik053@gmail.com>
2023-10-23 08:37:38 +0530

    Create generate_value.ps1

---
+++ generate_value.ps1
@@ -0,0 +1,22 @@
+$sequence_code = "syn"
+$app_env = "dev"
+Write-Verbose "Looking for a synapse workspace name which contains $sequence_code and $app_env"
+$SynapseWorkspace = Get-AzSynapseWorkspace | Where-Object {($_.Name -match "$sequence_code") -and ($_.Name -match "$app_env-")}
+
+
+$myout = [PSCustomObject]@{
+    SynapseWorkspaceName = $SynapseWorkspace.Name
+}
+
+Write-Verbose $myout -Verbose
+
+if ($myout)
+{
+    $myout | Get-Member -MemberType NoteProperty | ForEach-Object {
+        "$($_.Name)=$($myout.$($_.Name))"
+    } | Out-File -FilePath $Env:GITHUB_ENV -Append
+}
+else
+{
+    Write-Error "Couldn't find a SynapseWorkspaceName name which contains $sequence_code and $app_env" -ErrorAction Stop
+}
